6baf1a5e04b49626320e39ecbc3813733ea18146
IPECTER <ipectert@gmail.com>
2023-05-08 23:48:07 +0900

    Edit

--- build.rs
+++ build.rs
@@ -3,12 +3,12 @@ fn main() {
     if cfg!(windows) {
         let result = winres::WindowsResource::new()
             .set_icon("res/icon.ico")
-            .set("ProductName", "Fabric Installer")
-            .set("CompanyName", "Fabric Team")
-            .set("LegalCopyright", "Apache License Version 2.0")
+            .set("ProductName", "RTUClient Installer")
+            .set("CompanyName", "RTU SERVER & Fabric Team")
+            .set("FileDescription", "RTUClient Installer With Fabric")
+            .set("LegalCopyright", "Copyright (C) 2023")
             .set_manifest_file("platform/windows/program.manifest")
             .compile();
-
         if let Err(_) = result {
             panic!("Failed to set windows resources");
         }
